7c9d720c6986b5081b3e58266f279ca7e33d853c
Adel Sibgatullin <adelsibga@gmail.com>
2023-02-27 11:22:36 +0300

    Правки по коду

--- lab1/task1/script.js
+++ lab1/task1/script.js
@@ -1,57 +1,15 @@
-const canvas = document.querySelector('#canvas')
-const ctx = canvas.getContext('2d')
-const cellDimensions = 50
+const firstLetter = document.querySelector('.first-letter')
+const secondLetter = document.querySelector('.second-letter')
+const thirdLetter = document.querySelector('.third-letter')
 
-const drawLetter = () => {
-    const Letter = new Path2D()
-    Letter.rect(2 * cellDimensions, cellDimensions, 2 * cellDimensions, cellDimensions)
-    Letter.rect(cellDimensions, cellDimensions, cellDimensions, 5 * cellDimensions)
-    Letter.rect(2 * cellDimensions, 5 * cellDimensions, 2 * cellDimensions, cellDimensions)
-    ctx.fillStyle = '#93e69f'
-    ctx.fill(Letter)
+const setRectilinearUniformlyAcceleratedMotion = () => {
+    const v = 50
+    const v0 = 50
+    const t = 3000
+    const delay = 300
+    
+    let a = (v - v0)/t
+    console.log(a)
 }
 
-const drawSecondLetter = () => {
-    ctx.beginPath()
-    ctx.moveTo(8 * cellDimensions, cellDimensions)
-    ctx.lineTo(7 * cellDimensions, cellDimensions)
-    ctx.lineTo(5 * cellDimensions, 6 * cellDimensions)
-    ctx.lineTo(6 * cellDimensions, 6 * cellDimensions)
-    ctx.lineTo(7 * cellDimensions, 4 * cellDimensions)
-    ctx.lineTo(8 * cellDimensions, 4 * cellDimensions)
-    ctx.lineTo(9 * cellDimensions, 6 * cellDimensions)
-    ctx.lineTo(10 * cellDimensions, 6 * cellDimensions)
-    ctx.lineTo(8 * cellDimensions, cellDimensions)
-    ctx.fillStyle = '#9b52b5'
-    ctx.fill()
-
-    ctx.beginPath()
-    ctx.fillStyle = '#fff'
-    ctx.lineTo(7.5 * cellDimensions, 2 * cellDimensions)
-    ctx.lineTo(7 * cellDimensions, 3 * cellDimensions)
-    ctx.lineTo(8 * cellDimensions, 3 * cellDimensions)
-    ctx.lineTo(7.5 * cellDimensions, 2 * cellDimensions)
-    ctx.fill()
-}
-
-const drawThirdLetter = () => {
-    const Letter = new Path2D()
-    Letter.rect(11 * cellDimensions, cellDimensions, cellDimensions, 5 * cellDimensions)
-
-    ctx.beginPath()
-    ctx.moveTo(12 * cellDimensions, 6 * cellDimensions)
-    ctx.lineTo(15 * cellDimensions, 2 * cellDimensions)
-    ctx.lineTo(15 * cellDimensions, cellDimensions)
-    ctx.lineTo(12 * cellDimensions, 5 * cellDimensions)
-    ctx.fillStyle = '#f5f'
-    ctx.fill()
-
-    Letter.rect(15 * cellDimensions, cellDimensions, cellDimensions, 5 * cellDimensions)
-
-    ctx.fillStyle = '#f5f'
-    ctx.fill(Letter)
-}
-
-drawLetter()
-drawSecondLetter()
-drawThirdLetter()
+setRectilinearUniformlyAcceleratedMotion()
