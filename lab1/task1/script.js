e0567bc657826a54493ae6551c3b711304c9046e
Adel Sibgatullin <adelsibga@gmail.com>
2023-03-01 16:34:42 +0300

    Переделал на полигоны Поправил анимацию прыжка Поправил стили

--- lab1/task1/script.js
+++ lab1/task1/script.js
@@ -1,8 +1,8 @@
 const letters = document.querySelectorAll('svg')
 
 const ANIMATION_NAME = 'animateLetter'
-const ANIMATION_TIME_IN_SEC = 3
-const ANIMATION_PROP = 'infinite'
+const ANIMATION_TIME_IN_SEC = 1
+const ANIMATION_PROPS = 'cubic-bezier(0.215, 0.61, 0.355, 1) alternate infinite'
 
 function animation() {
     let letterIndex = 0
@@ -12,7 +12,7 @@ function animation() {
             clearInterval(interval)
         }
         
-        letters[letterIndex++].style.animation = `${ANIMATION_NAME} ${ANIMATION_TIME_IN_SEC}s ${ANIMATION_PROP}`
+        letters[letterIndex++].style.animation = `${ANIMATION_NAME} ${ANIMATION_TIME_IN_SEC}s ${ANIMATION_PROPS}`
     }, 300)
 }
 
