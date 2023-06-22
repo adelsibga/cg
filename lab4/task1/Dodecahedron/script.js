94b59a1c4aaf97d26bf7807a4ad887c852553bc7
Adel Sibgatullin <adelsibga@gmail.com>
2023-06-22 05:45:26 +0300

    lab4

--- lab4/task1/Dodecahedron/script.js
+++ lab4/task1/Dodecahedron/script.js
@@ -1,24 +1,20 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.136.0'
 import {OrbitControls} from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls'
 
-// TODO: добавить освещение
 // TODO: добавить цвет для фигуры
-// TODO: добавить прозрачность для фигуры
 // TODO: добавить ресайз окна и смешение фигуры
 
 const WIDTH = window.innerWidth
 const HEIGHT = window.innerHeight
 const RADIUS = 200
+const BLACK_COLOR = 0x000000
+const WHITE_COLOR = 0xffffff
 
 let scene = new THREE.Scene()
-scene.background = new THREE.Color(0xffffff)
+scene.background = new THREE.Color(WHITE_COLOR)
 
 let camera = new THREE.PerspectiveCamera(40, WIDTH / HEIGHT, 1, 10000)
 camera.position.set(250, 30, 800)
-
-// const light = new THREE.DirectionalLight(0xffffff)
-// light.position.set(0, 0, 1)
-// scene.add(light)
 
 let renderer = new THREE.WebGLRenderer()
 renderer.setSize(WIDTH, HEIGHT)
@@ -27,14 +23,25 @@ document.body.appendChild(renderer.domElement)
 const controls = new OrbitControls(camera, renderer.domElement)
 
 const geometry = new THREE.DodecahedronGeometry(RADIUS, 0)
-const material = new THREE.MeshBasicMaterial({
-    color: 0x000000,
-    wireframe: true
+const material = new THREE.MeshPhongMaterial({
+    color: '#f5f',
+    transparent: true, opacity: 0.6
 })
 
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
+const materialWireframe = new THREE.MeshPhongMaterial({
+    color: BLACK_COLOR,
+    wireframe: true
+})
+
+const meshColor = new THREE.Mesh(geometry, materialWireframe)
+scene.add(meshColor)
+
+const light = new THREE.DirectionalLight(WHITE_COLOR, 1)
+scene.add(light)
+light.position.set(3, 3, 3)
 
 function animate() {
     requestAnimationFrame(animate)
